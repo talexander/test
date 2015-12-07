42bf1088fd9505d03f200a8d5b6099f3e6cdc9d6
Александр Тараканов <aleksandrtarakanov@msk-wifi-6fap0-a_tarakanov-a.tarakanov.mail.msk>
2015-12-07 16:25:38 +0300

    cian grabber

---
+++ cian/cian/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CianItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    rent = scrapy.Field()
+    fee = scrapy.Field()
+    underground = scrapy.Field()
+    street = scrapy.Field()
+    link = scrapy.Field()
+    descr = scrapy.Field()
---
+++ cian/cian/spiders/adv.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from cian.items import CianItem
+
+
+class AdvSpider(scrapy.Spider):
+    name = "adv"
+    allowed_domains = ["cian.ru"]
+    start_urls = (
+    	'http://www.cian.ru/cat.php?currency=2&deal_type=rent&engine_version=2&foot_min=25&maxprice=35000&metro%5B0%5D=9&metro%5B1%5D=36&metro%5B2%5D=116&minfloor=2&minprice=27000&offer_type=flat&only_foot=2&room1=1&type=-2&wp=1',
+    )
+
+    def parse(self, response):
+	for elem in response.selector.css('.serp-list .serp-item'):
+		item = CianItem()
+		item['rent'] = elem.css('.serp-item__price-col .serp-item__solid::text').extract_first().encode('utf-8')
+		item['fee'] = elem.css('.serp-item__price-col .serp-item__prop').extract()
+		item['underground'] = elem.css('.serp-item__metro a').extract()
+		item['street'] = elem.css('.serp-item__address-precise a').extract()
+		item['link'] = elem.css('.serp-item__card-link::attr(href)').extract()
+		item['descr'] = elem.css('.serp-item__description__text').extract()
+		yield item
+
