381355384b1d467d2724955f72ccbe8f8cc88763
Александр Тараканов <atarakanoff@gmail.com>
2015-12-08 11:52:59 +0300

    format items

--- cian/cian/items.py
+++ cian/cian/items.py
@@ -11,9 +11,9 @@ import scrapy
 class CianItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    rent = scrapy.Field()
-    fee = scrapy.Field()
+    link = scrapy.Field()
     underground = scrapy.Field()
     street = scrapy.Field()
-    link = scrapy.Field()
+    rent = scrapy.Field()
+    fee = scrapy.Field()
     descr = scrapy.Field()
